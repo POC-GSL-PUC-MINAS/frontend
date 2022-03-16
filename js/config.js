f579e000edf863ed12241950ba3ae660707dabbc
Evandro Almeida <89evandroalmeida@gmail.com>
2022-03-16 13:59:45 -0300

    Conectando o app ao Cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ia9T24hQr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2jaaafhc20pd90afibtj5nj8r7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
